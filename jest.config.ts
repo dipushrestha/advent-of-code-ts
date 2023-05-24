94e7c8b75564349ab2740a88c68f257dc58546e9
Dipendra Shrestha <dipendrashrestha059@gmail.com>
2023-05-25 00:40:50 +0545

    test: configure jest coverage

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,17 @@ import type { JestConfigWithTsJest } from 'ts-jest';
 
 const jestConfigWithTsJest: JestConfigWithTsJest = {
   preset: 'ts-jest',
-  testEnvironment: 'node'
+  testEnvironment: 'node',
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  }
 };
 
 module.exports = jestConfigWithTsJest;
