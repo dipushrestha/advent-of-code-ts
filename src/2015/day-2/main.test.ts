ff43a062749df0b7c6d667abc44ffd0b40105f83
Dipendra Shrestha <dipendrashrestha059@gmail.com>
2023-05-28 22:28:27 +0545

    day 2, part 2 solution

--- src/2015/day-2/main.test.ts
+++ src/2015/day-2/main.test.ts
@@ -1,11 +1,23 @@
 import { describe, test, expect } from '@jest/globals';
-import { getRequiredWrappingPaperForPresent } from './main';
+import {
+  getRequiredRibbonForPresent,
+  getRequiredWrappingPaperForPresent
+} from './main';
 
 describe('Day 2: I Was Told There Would Be No Math', () => {
   test('A present with dimensions 2x3x4 requires 2*6 + 2*12 + 2*8 = 52 square feet of wrapping paper plus 6 square feet of slack, for a total of 58 square feet', () => {
     expect(getRequiredWrappingPaperForPresent('2x3x4')).toBe(58);
   });
+
   test('A present with dimensions 1x1x10 requires 2*1 + 2*10 + 2*10 = 42 square feet of wrapping paper plus 1 square foot of slack, for a total of 43 square feet', () => {
     expect(getRequiredWrappingPaperForPresent('1x1x10')).toBe(43);
+  });
+
+  test('A present with dimensions 2x3x4 requires 2+2+3+3 = 10 feet of ribbon to wrap the present plus 2*3*4 = 24 feet of ribbon for the bow, for a total of 34 feet', () => {
+    expect(getRequiredRibbonForPresent('2x3x4')).toBe(34);
+  });
+
+  test('A present with dimensions 1x1x10 requires 1+1+1+1 = 4 feet of ribbon to wrap the present plus 1*1*10 = 10 feet of ribbon for the bow, for a total of 14 feet', () => {
+    expect(getRequiredRibbonForPresent('1x1x10'));
   });
 });
--- src/2015/day-2/main.ts
+++ src/2015/day-2/main.ts
@@ -4,11 +4,19 @@ const allPresentDimensions = fs
   .readFileSync(`${__dirname}/input.txt`, 'utf-8')
   .split('\n');
 
-export function getRequiredWrappingPaperForPresent(dimensions: string): number {
+function getDimensionsAsNumbers(
+  dimensions: string
+): [length: number, width: number, height: number] {
   const splitDimensions = dimensions.split('x') as [string, string, string];
   const length = parseInt(splitDimensions[0]);
   const width = parseInt(splitDimensions[1]);
   const height = parseInt(splitDimensions[2]);
+
+  return [length, width, height];
+}
+
+export function getRequiredWrappingPaperForPresent(dimensions: string): number {
+  const [length, width, height] = getDimensionsAsNumbers(dimensions);
 
   const lwArea = length * width;
   const whArea = width * height;
@@ -20,14 +28,36 @@ export function getRequiredWrappingPaperForPresent(dimensions: string): number {
   return surfaceArea + smallestSideArea;
 }
 
+export function getRequiredRibbonForPresent(dimensions: string): number {
+  const [length, width, height] = getDimensionsAsNumbers(dimensions);
+
+  const lwPerimeter = 2 * (length + width);
+  const whPerimeter = 2 * (width + height);
+  const hlPerimeter = 2 * (height + length);
+
+  const wrapRibbonLength = Math.min(lwPerimeter, whPerimeter, hlPerimeter);
+  const bowRibbonLength = length * width * height;
+
+  return wrapRibbonLength + bowRibbonLength;
+}
+
 let totalWrappingPaperRequiredForAllPresents = 0;
+let totalRibbonRequiredForAllPresents = 0;
 
 for (let presentDimensions of allPresentDimensions) {
   totalWrappingPaperRequiredForAllPresents +=
     getRequiredWrappingPaperForPresent(presentDimensions);
+
+  totalRibbonRequiredForAllPresents +=
+    getRequiredRibbonForPresent(presentDimensions);
 }
 
 console.log(
   'Day 2 -> Part 1 -> Answer(Total required square feet of wrapping paper):',
   totalWrappingPaperRequiredForAllPresents
+);
+
+console.log(
+  'Day 2 -> Part 2 -> Answer(Total required feet of ribbon):',
+  totalRibbonRequiredForAllPresents
 );
