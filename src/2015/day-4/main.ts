4f41b3d367df3d1d713b13ab02caf72157970551
Dipendra Shrestha <dipendrashrestha059@gmail.com>
2023-05-30 16:28:14 +0545

    day 4, part 1 and part 2 solutions

---
+++ src/2015/day-4/main.ts
@@ -0,0 +1,30 @@
+import { createHash } from 'crypto';
+
+export function findLowestNumberCreatingMD5HashWithLeadingZeros(
+  secretKey: string,
+  zeroesCount: number
+): number {
+  let md5Hash = '';
+  let keyNumber = 0;
+
+  while (!md5Hash.startsWith('0'.repeat(zeroesCount))) {
+    keyNumber += 1;
+    md5Hash = createHash('md5')
+      .update(secretKey + keyNumber)
+      .digest('hex');
+  }
+
+  return keyNumber;
+}
+
+const puzzleSecretKey = 'iwrupvqb';
+
+console.log(
+  'Day 4 -> Part 1 -> Answer(Lowest positive number generating MD5 hash having leading five zeroes:',
+  findLowestNumberCreatingMD5HashWithLeadingZeros(puzzleSecretKey, 5)
+);
+
+console.log(
+  'Day 4 -> Part 2 -> Answer(Lowest positive number generating MD5 hash having leading six zeroes:',
+  findLowestNumberCreatingMD5HashWithLeadingZeros(puzzleSecretKey, 6)
+);
