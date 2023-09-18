dffa0492f98ad75ec753cf0aa91ffc29c917d8e5
Dipendra Shrestha <dipendrashrestha059@gmail.com>
2023-09-18 14:06:48 +0545

    feat: 2015 day 12 part 2 solution

--- src/2015/day-12/main.test.ts
+++ src/2015/day-12/main.test.ts
@@ -1,5 +1,8 @@
 import { describe, test, expect } from '@jest/globals';
-import { calculateSumOfAllNumbersInJSON } from './main';
+import {
+  calculateSumOfAllNumbersExceptRedInJSON,
+  calculateSumOfAllNumbersInJSON
+} from './main';
 
 describe('Day 12: JSAbacusFramework.io', () => {
   test('[1,2,3] and {"a":2,"b":4} both have a sum of 6', () => {
@@ -20,5 +23,29 @@ describe('Day 12: JSAbacusFramework.io', () => {
   test('[] and {} both have a sum of 0', () => {
     expect(calculateSumOfAllNumbersInJSON([])).toBe(0);
     expect(calculateSumOfAllNumbersInJSON({})).toBe(0);
+  });
+
+  test('[1,2,3] still has a sum of 6', () => {
+    expect(calculateSumOfAllNumbersExceptRedInJSON([1, 2, 3])).toBe(6);
+  });
+
+  test('[1,{"c":"red","b":2},3] now has a sum of 4, because the middle object is ignored', () => {
+    expect(
+      calculateSumOfAllNumbersExceptRedInJSON([1, { c: 'red', b: 2 }, 3])
+    ).toBe(4);
+  });
+
+  test('{"d":"red","e":[1,2,3,4],"f":5} now has a sum of 0, because the entire structure is ignored', () => {
+    expect(
+      calculateSumOfAllNumbersExceptRedInJSON({
+        d: 'red',
+        e: [1, 2, 3, 4],
+        f: 5
+      })
+    ).toBe(0);
+  });
+
+  test('[1,"red",5] has a sum of 6, because "red" in an array has no effect', () => {
+    expect(calculateSumOfAllNumbersExceptRedInJSON([1, 'red', 5])).toBe(6);
   });
 });
--- src/2015/day-12/main.ts
+++ src/2015/day-12/main.ts
@@ -1,8 +1,26 @@
 import input from './input.json';
 
-function traverseJSON(json: unknown, callback: (value: unknown) => void) {
+function traverseJSON({
+  json,
+  callback,
+  excludePropertyHaving
+}: {
+  json: unknown;
+  callback: (value: unknown) => void;
+  excludePropertyHaving?: string;
+}) {
   if (json !== null && typeof json === 'object') {
-    Object.values(json).forEach((value) => traverseJSON(value, callback));
+    if (
+      excludePropertyHaving &&
+      !Array.isArray(json) &&
+      Object.values(json).includes(excludePropertyHaving)
+    ) {
+      return;
+    }
+
+    Object.values(json).forEach((value) =>
+      traverseJSON({ json: value, callback, excludePropertyHaving })
+    );
   } else {
     callback(json);
   }
@@ -10,9 +28,25 @@ function traverseJSON(json: unknown, callback: (value: unknown) => void) {
 
 export function calculateSumOfAllNumbersInJSON(json: unknown): number {
   let sum = 0;
-  traverseJSON(json, (value) => {
-    if (typeof value !== 'number') return;
-    sum += value;
+  traverseJSON({
+    json,
+    callback: (value) => {
+      if (typeof value !== 'number') return;
+      sum += value;
+    }
+  });
+  return sum;
+}
+
+export function calculateSumOfAllNumbersExceptRedInJSON(json: unknown): number {
+  let sum = 0;
+  traverseJSON({
+    json,
+    callback: (value) => {
+      if (typeof value !== 'number') return;
+      sum += value;
+    },
+    excludePropertyHaving: 'red'
   });
   return sum;
 }
@@ -20,4 +54,9 @@ export function calculateSumOfAllNumbersInJSON(json: unknown): number {
 console.log(
   'Day 12 -> Part 1 -> Answer(Sum of all number values in json):',
   calculateSumOfAllNumbersInJSON(input)
+);
+
+console.log(
+  'Day 12 -> Part 2 -> Answer(Sum of all number values in json except red props):',
+  calculateSumOfAllNumbersExceptRedInJSON(input)
 );
